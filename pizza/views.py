d833fcef2fe8b102f94c05f3628529ed88d5b52b
daniel-kav <dankavatha01@gmail.com>
2024-09-02 20:11:30 +0300

    made home and menu view

--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request , 'pizza/home.html')
+
+def menu(request):
+    return render(request , 'pizza/menu.html')
