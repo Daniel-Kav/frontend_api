140fc4fd9b8d8f34a6caa6980ea5b10042b53d73
daniel-kav <dankavatha01@gmail.com>
2024-09-02 20:55:04 +0300

    added a form and bootstrap

--- pizza/views.py
+++ pizza/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from .forms import PizzaForm
 # Create your views here.
 def home(request):
     return render(request , 'pizza/home.html')
 
 def order(request):
-    return render(request , 'pizza/order.html')
+    return render(request , 'pizza/order.html', {'orderform':PizzaForm()})
