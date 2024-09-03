dd670f7342a92eca3209bfd7e6a3665add697676
daniel-kav <dankavatha01@gmail.com>
2024-09-03 20:31:02 +0300

    fixed the views

--- todoApp_Proj/tasks/views.py
+++ todoApp_Proj/tasks/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def TaskList(request):
-    return render(request, 'task_list.html',{}   )
+    return render(request, 'tasks/task_list.html',{}   )
