3ebb3a80ea92ef1b910d8eb96aa007f4c6a26e9c
daniel-kav <dankavatha01@gmail.com>
2024-09-03 18:42:24 +0300

    saving data to the database

--- pizza/views.py
+++ pizza/views.py
@@ -9,6 +9,7 @@ def order(request):
     multi_form = MultiplePizzaForm()
     if request.method == 'POST':
         form_data = PizzaForm(request.POST)
+        form_data.save()
         if form_data.is_valid():
             note = "Thanks for ordering. Your %s %s %s pizza order is enroute." % (form_data.cleaned_data['size'], form_data.cleaned_data['topping1'], form_data.cleaned_data['topping2'])
             return   render(request , 'pizza/order.html', {'orderform':PizzaForm(), "note" : note, "multi_form" :multi_form})
@@ -16,7 +17,7 @@ def order(request):
         return   render(request , 'pizza/order.html', {'orderform':PizzaForm(), 'multi_form':multi_form})
     
 
-def pizzas(self, request):
+def pizzas(request):
     number_of_pizzas = 2
     filled_multi_form = MultiplePizzaForm(request.GET)
     if filled_multi_form.is_valid():
