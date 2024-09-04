1692e4cc0d9e7ee05550bff6deb0ad10f002ed8e
daniel-kav <dankavatha01@gmail.com>
2024-09-04 19:28:13 +0300

    delete url

--- todoApp_Proj/tasks/urls.py
+++ todoApp_Proj/tasks/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.TaskList, name='task_list'),
     path('edit/<int:pk>', views.TaskUpdate, name='task_update'),
+    path('delete/<int:pk>', views.TaskDelete, name='task_delete'),
+
 ]
