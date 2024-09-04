a8cb3ed2e91ddbeff32ef8a01b6b2914aeaac404
daniel-kav <dankavatha01@gmail.com>
2024-09-04 20:14:26 +0300

    login template

--- todoApp_Proj/tasks/urls.py
+++ todoApp_Proj/tasks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.TaskList, name='task_list'),
     path('edit/<int:pk>', views.TaskUpdate, name='task_update'),
     path('delete/<int:pk>', views.TaskDelete, name='task_delete'),
+    path('register', views.Register, name='register')
 
 ]
