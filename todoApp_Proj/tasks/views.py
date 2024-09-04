fc1601d666fe89d21ef5651cc01ca7a2eb12474e
daniel-kav <dankavatha01@gmail.com>
2024-09-04 19:57:12 +0300

    add auth logic to all views

--- todoApp_Proj/tasks/views.py
+++ todoApp_Proj/tasks/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
 
 # Create your views here.
+@login_required
 def TaskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user = request.user)
     form = TaskForm()
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
             return redirect('task_list')
     return render(request, 'tasks/task_list.html', {'tasks': tasks, 'form': form})
 
+@login_required
 def TaskUpdate(request, pk):
-    task = Task.objects.get(id=pk)
+    task = get_object_or_404(Task, pk=pk, user=request.user)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
@@ -24,8 +29,9 @@ def TaskUpdate(request, pk):
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_update.html', {'form': form})
 
+@login_required
 def TaskDelete(request, pk):
-    task = get_object_or_404(Task, id=pk)
+    task = get_object_or_404(Task, id=pk, user=request.user)
     if request.method == 'POST':
         task.delete()
         return redirect('task_list')
