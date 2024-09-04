5e61c358daac4118e9114550292ea073c320fe80
daniel-kav <dankavatha01@gmail.com>
2024-09-04 18:51:57 +0300

    updated successfully

--- todoApp_Proj/tasks/views.py
+++ todoApp_Proj/tasks/views.py
@@ -14,12 +14,12 @@ def TaskList(request):
     return render(request, 'tasks/task_list.html', {'tasks': tasks, 'form': form})
 
 def TaskUpdate(request, pk):
-    task = Task.objects.get(pk=pk)
+    task = Task.objects.get(id=pk)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
             return redirect('task_list')
-        else:
-            form = TaskForm(instance=task )
+    else:
+        form = TaskForm(instance=task)
     return render(request, 'tasks/task_update.html', {'form': form})
