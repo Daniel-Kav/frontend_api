f397ece918ad776d37d4880ea7da9a8bd7b64f3d
daniel-kav <dankavatha01@gmail.com>
2024-09-05 17:23:37 +0300

    done with creating blogs and sending them to the database

--- blog_project/blog/forms.py
+++ blog_project/blog/forms.py
@@ -2,5 +2,11 @@ from django import forms
 from .models import Blog
 
 class BlogForm(forms.ModelForm):
-    model = Blog
-    fields = ['title', 'description']
+    class Meta:
+        model = Blog
+        fields = ['title', 'description']
+
+# class BlogForm(forms.ModelForm):
+#     class Meta:
+#         model = Blog
+#         fields = ['username','email', 'password1', 'password2']
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
+from .forms import BlogForm
 
 # Create your views here.
 def BlogListView(request):
-    return render(request,'blog/blog_list.html', {})
+    Blog_list = Blog.objects.all()
+    form = BlogForm()
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'blog/blog_list.html', {"bloglist":Blog_list,'form':form })
