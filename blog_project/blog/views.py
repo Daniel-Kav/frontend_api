63731b028b74187a4bf8be4cbd92780dd9926eb2
daniel-kav <dankavatha01@gmail.com>
2024-09-06 12:56:56 +0300

    bugs fixed blog CRUD functining

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -36,10 +36,10 @@ def BlogDelete(request,pk):
     return render(request, 'blog/blog_delete_confirmation.html', {'blog': blog})
 
 def post_list(request):
-    posts = Post.objects.all().order_by('created_at')
-    return render(request, 'blog/blog_list.html', {'posts': posts})
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request,pk):
+def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.all()
     if request.method == 'POST':
@@ -49,10 +49,10 @@ def post_detail(request,pk):
             comment.author = request.user
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk= post.pk)
-        else:
-            comment_form = CommentForm()
-    return render(request, 'blog/post_detail.html',{'comments': comments, 'post': post})
+            return redirect('post_detail', pk=post.pk)
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
 
 @login_required
 def post_new(request):
@@ -62,7 +62,7 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk= post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
@@ -71,18 +71,18 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.user != post.author:
-        return redirect('post_detail', pk = pk)
+        return redirect('post_detail', pk=pk)
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
-        if  form.is_valid():
+        if form.is_valid():
             post = form.save()
-            return redirect('post_detail', pk = post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm( instance=post)
-    return render(request, 'blog/post_detail.html', {'form': form})
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
-def post_delete(request , pk):
+def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.user == post.author:
         post.delete()
