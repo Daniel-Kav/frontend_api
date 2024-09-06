7e964f43c11ab348de238494d00ad85c0922e29f
daniel-kav <dankavatha01@gmail.com>
2024-09-06 11:25:51 +0300

    update urls fixed

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.BlogListView, name='blog-list'),
     path('details/<int:pk>', views.BlogDetails, name='blog_details'),
+    path('edit/<int:pk>', views.BlogUpdate, name='blog_update'),
     path('delete/<int:pk>', views.BlogDelete, name='blog_delete'),
 ]
