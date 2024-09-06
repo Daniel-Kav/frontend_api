91858a9f78dcbf23aa799f33a9a7f822da01d469
daniel-kav <dankavatha01@gmail.com>
2024-09-06 12:43:58 +0300

    post listing template

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BlogListView, name='blog-list'),
-    path('details/<int:pk>', views.BlogDetails, name='blog_details'),
-    path('edit/<int:pk>', views.BlogUpdate, name='blog_update'),
-    path('delete/<int:pk>', views.BlogDelete, name='blog_delete'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detail, name='post_details'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+
 ]
