4a895a495b2b29047df7ae3e8b8bba2240939e26
daniel-kav <dankavatha01@gmail.com>
2024-09-06 20:31:49 +0300

    registered job model

--- portfolio_proj/portfolio_proj/settings.py
+++ portfolio_proj/portfolio_proj/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'adminadmin',
+        'PORT': '5432',
+        'HOST':'localhost',
     }
 }
 
