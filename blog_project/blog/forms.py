d89e4762d4acbd0747724b683931d11ad0938bba
daniel-kav <dankavatha01@gmail.com>
2024-09-07 17:53:44 +0300

    handled the profile form

--- blog_project/blog/forms.py
+++ blog_project/blog/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import Blog,Post,Comment
+from .models import Blog,Post,Comment,Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
 
 class BlogForm(forms.ModelForm):
     class Meta:
@@ -24,3 +25,8 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email', 'password1','password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','location', 'date_of_birth']
