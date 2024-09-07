70e5a98f00ef8e5aa4a94e125b971e99a17b354c
daniel-kav <dankavatha01@gmail.com>
2024-09-07 12:32:51 +0300

    workingon xpounding onthe jobs

--- portfolio_proj/jobs/urls.py
+++ portfolio_proj/jobs/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('jobs/<int:job_id>',views.job_detail, name='detail'),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- portfolio_proj/jobs/views.py
+++ portfolio_proj/jobs/views.py
@@ -7,6 +7,6 @@ def home(request):
     return render(request, 'jobs/home.html', {'jobs': jobs})
 
 def job_detail(request, job_id):
-    job_detail = get_object_or_404(Job , pk = job_id)
-    return render(request, 'jobs/detail.html', {'job_detail': job_detail})
+    job = get_object_or_404(Job , pk = job_id)
+    return render(request, 'jobs/detail.html', {'job': job})
 
