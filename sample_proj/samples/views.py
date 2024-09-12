9e345e06b5f3f12a021cd89a0f6531fcf61e4344
daniel-kav <dankavatha01@gmail.com>
2024-09-12 22:59:44 +0300

    successfully handled read operations

--- sample_proj/samples/views.py
+++ sample_proj/samples/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,redirect
+from .models import Sample
+from .forms import SampleForm
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'samples/home.html',{})
+    samples = Sample.objects.all()
+
+    if request.method == 'POST':
+        form = SampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+
+
+    return render(request, 'samples/home.html',{"samples":samples, "form":form})
+
+
