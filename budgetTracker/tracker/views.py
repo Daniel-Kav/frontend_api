cfe210cb23f706bf25031c4b7c1f7d1b8f107ab6
daniel-kav <dankavatha01@gmail.com>
2024-09-14 21:28:13 +0300

    darshboard  view

--- budgetTracker/tracker/views.py
+++ budgetTracker/tracker/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ExpenseForm,IncomeForm
+from .models import Expense,Category,Income
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def darshboard(request):
+    incomes = Income.objects.filter(user = request.user)
+    expenses = Expense.objects.filter(user = request.user)
+    total_expenses = sum(expense.amount for expense in expenses)
+    total_income = sum(income.amount for income in incomes)
+    balance = total_income - total_expenses
+    context = {
+        'incomes': incomes,
+        'balance': balance,
+        'expenses': expenses,
+        'incomes': incomes,
+        'total_expenses': total_expenses,
+        'total_income': total_income,
+    }
+
+    return render(request, 'tracker/dashboard.html')
+    
