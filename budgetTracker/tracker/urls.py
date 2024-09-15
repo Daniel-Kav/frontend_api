28648e6f740fd3ee95d743f59739a1387d61381c
daniel-kav <dankavatha01@gmail.com>
2024-09-15 19:10:51 +0300

    fixed del issues

--- budgetTracker/tracker/urls.py
+++ budgetTracker/tracker/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('add-income/', views.add_income, name='add_income'),
     path('add-expense/', views.add_expense, name='add_expense'),
-    path('del-income/', views.del_income, name='del_income'),
+    path('del-income/<int:pk>', views.del_income, name='del_income'),
 ]
