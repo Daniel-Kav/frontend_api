fcab631abfebf813a7fb5e9a75aac73a3ec0d48a
daniel-kav <dankavatha01@gmail.com>
2024-09-15 20:18:44 +0300

    del exp template

--- budgetTracker/tracker/urls.py
+++ budgetTracker/tracker/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add-income/', views.add_income, name='add_income'),
     path('add-expense/', views.add_expense, name='add_expense'),
     path('del-income/<int:pk>', views.del_income, name='del_income'),
+    path('del-expense/<int:pk>', views.del_expense, name='del_expense'),
 ]
