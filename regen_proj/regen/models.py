759f8d48abb5f3d0aece7166ddfcceea84018c71
daniel-kav <dankavatha01@gmail.com>
2024-09-16 18:44:12 +0300

    models clas

--- regen_proj/regen/models.py
+++ regen_proj/regen/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=400)
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return self.description
+
+class Income(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.description
