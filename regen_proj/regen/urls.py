6c24ac2ca188e129c9a1ebab53a0a3353fbff584
daniel-kav <dankavatha01@gmail.com>
2024-09-16 19:26:08 +0300

    darsh done

--- regen_proj/regen/urls.py
+++ regen_proj/regen/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from django
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns =[
     path('', views.dashboard, name='dashboard'),
     path('add-income', views.add_income, name='add_income'),
     path('add-expense', views.add_expenses, name='add_expense'),
-    path('login', views.login, name='login'),
+    path('login', LoginView.as_view(template_name = 'regen/login'), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
