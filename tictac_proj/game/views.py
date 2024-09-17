5775f89f07a270b41f561984b9031a68cb7539c2
daniel-kav <dankavatha01@gmail.com>
2024-09-17 17:25:28 +0300

    check winner view

--- tictac_proj/game/views.py
+++ tictac_proj/game/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+board  = [""] * 9
+
+def check_winner(board):
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal wins
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical wins
+        [0, 4, 8], [2, 4, 6],             # Diagonal wins
+    ]
+    for condition in win_conditions:
+        if board[condition[0]] == board[condition[1]] and board[condition[2]] and board[condition[0]] != [""]:
+            return board[condition[0]]
+        return None
