7693014f73731fa1b00cfcf01d6ae2954ceeb4d2
daniel-kav <dankavatha01@gmail.com>
2024-09-17 18:10:24 +0300

    test for homepage passed

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+import pytest
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_home_view(client):
+    print("Testing view for home page...")
+    response = client.get(path='/')
+    assert response.status_code == 200
