0f6a8bc99343903f0da40b6f26ef27f3ef2a6585
daniel-kav <dankavatha01@gmail.com>
2024-09-17 18:34:41 +0300

    auth test

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 import pytest
 
 # Create your tests here.
@@ -12,4 +13,13 @@ def test_login_view(client):
     response = client.get(path='/accounts/login/')
     assert response.status_code == 200
     assert 'Login' in str(response.content)
-    assert ''
+    assert 'registration/login.html' in response.template_name
+
+@pytest.mark.django_db
+def test_sigup_authentication(client):
+    user  = User.objects.create_user('testuser','test@example.com', 'django123')
+    client.login(username = user.username, password = 'django123')
+
+    response = client.get(path ='/register/', follow  = True)
+    assert response.status_code == 200
+    assert 'Register' in str(response.content)
