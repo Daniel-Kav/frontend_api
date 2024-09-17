2e45aba4e1b47664c49f3b82f1a8518f4e57ab7b
daniel-kav <dankavatha01@gmail.com>
2024-09-17 18:20:11 +0300

    writing tests for the loginpage

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -7,3 +7,9 @@ def test_home_view(client):
     print("Testing view for home page...")
     response = client.get(path='/')
     assert response.status_code == 200
+
+def test_login_view(client):
+    response = client.get(path='/accounts/login/')
+    assert response.status_code == 200
+    assert 'Login' in str(response.content)
+    assert ''
