33da919296c41d7e864014574a0ce871fd7c6840
daniel-kav <dankavatha01@gmail.com>
2024-09-18 18:45:27 +0300

    course model

--- school_lms/lms/models.py
+++ school_lms/lms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Course(models.Model):
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
