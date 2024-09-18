1aecb55827b94db02b72bc23b89d0e8ed3144617
daniel-kav <dankavatha01@gmail.com>
2024-09-18 19:10:53 +0300

    course list view

--- school_lms/lms/views.py
+++ school_lms/lms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import CoursesForm, LessonForm, QuizForm
+from .models import Course,Lesson,Quiz, Enrollment
 
 # Create your views here.
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'lms/course_list.html', {'courses': courses})
