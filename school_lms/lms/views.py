e86c94d1de27d74262a4c97c7c551afd78fc456b
daniel-kav <dankavatha01@gmail.com>
2024-09-19 00:07:41 +0300

    registration  view

--- school_lms/lms/views.py
+++ school_lms/lms/views.py
@@ -15,9 +15,10 @@ def course_details(request, pk):
 
 
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 class CourseListView(ListView):
     model = Course
@@ -38,4 +39,8 @@ class CustomLoginView(LoginView):
     template_name = 'lms/login.html'
     success_url = reverse_lazy('course_list')
 
+class RegistrationView(CreateView):
+    template_name = 'lms/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
 
