cf8e6417c4d70660ee5af18d874d90174347aab9
daniel-kav <dankavatha01@gmail.com>
2024-09-18 18:53:46 +0300

    quiz models

--- school_lms/lms/models.py
+++ school_lms/lms/models.py
@@ -22,4 +22,10 @@ class Lesson(models.Model):
     def __str__(self):
         return self.title
     
+class Quiz(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='quiz')
+    question = models.TextField()
+    answer = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.question
