4d212a602875362c1a0ed7bc064ee86e89207218
daniel-kav <dankavatha01@gmail.com>
2024-09-18 19:16:19 +0300

    couse detail view

--- school_lms/lms/views.py
+++ school_lms/lms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import CoursesForm, LessonForm, QuizForm
 from .models import Course,Lesson,Quiz, Enrollment
 
@@ -6,3 +6,9 @@ from .models import Course,Lesson,Quiz, Enrollment
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'lms/course_list.html', {'courses': courses})
+
+#couse details and lessons
+def course_details(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    lessons = course.lessons.all()
+    return render(request, 'lms/course_details.html',{'course':course, 'lessons':lessons})
