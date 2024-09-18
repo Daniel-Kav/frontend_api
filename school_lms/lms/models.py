1536a8602f56ffcc3644c16a9dcc9e6c1d2aa194
daniel-kav <dankavatha01@gmail.com>
2024-09-19 00:28:38 +0300

    changes

--- school_lms/lms/models.py
+++ school_lms/lms/models.py
@@ -16,7 +16,7 @@ class Course(models.Model):
 class Lesson(models.Model):
     course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
     title = models.CharField(max_length= 200)
-    content = models.RichTextField()
+    content = RichTextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
