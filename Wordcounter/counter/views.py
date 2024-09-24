39ba1b3caf7c3fe392e90da0620e40a1a63000e9
daniel-kav <dankavatha01@gmail.com>
2024-09-24 11:37:20 +0300

    counter done

--- Wordcounter/counter/views.py
+++ Wordcounter/counter/views.py
@@ -3,7 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 
 def home (request):
-    return render(request, 'counter/home.html')
+    if request.method == 'POST':
+        text = request.POST['text']
+
+        if text != '':
+            word = len(text.split())
+            i = True
+            return render(request, 'counter/home.html',
+                           {'word': word, 
+                            'text': text,
+                            'on': 'active'})
+        else:
+            return render(request, 'counter/home.html',  {'on': 'active'})
+    else:
+        return render(request, 'counter/home.html', {'on': 'active'})
 
 def counter(request):
-    
+    if request.method == 'POST':
+        text = request.POST['text']
+
+        if text != '':
+            word = len(text.split())
+            i = True
+            return render(request, 'counter/home.html',
+                           {'word': word, 
+                            'text': text,
+                            'on': 'active'})
+        else:
+            return render(request, 'counter/home.html',  {'on': 'active'})
+    else:
+        return render(request, 'counter/home.html', {'on': 'active'})
