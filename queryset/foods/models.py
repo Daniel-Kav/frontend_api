577e7dc936af2afa6a5ee063969c8b932e6d037d
daniel-kav <dankavatha01@gmail.com>
2024-09-25 18:26:24 +0300

    model addquery

--- queryset/foods/models.py
+++ queryset/foods/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
+    description = models.TextField()
+    cook = models.ForeignKey(User, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
