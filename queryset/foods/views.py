5a80d5f6f0d21b66f35f71dcf531f3f94dcb7a24
daniel-kav <dankavatha01@gmail.com>
2024-09-25 10:55:05 +0300

    class view

--- queryset/foods/views.py
+++ queryset/foods/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Food
+from django.views.generic import ListView
 
 # Create your views here.
 def  home(request):
     foods = Food.objects.all()
     return render(request, 'home.html', {'foods': foods})
+
+class HomeView(ListView):
+    model = Food
+    template_name = 'home.html'
+    queryset = Food.objects.filter(name__contains="b")
--- queryset/queryset/urls.py
+++ queryset/queryset/urls.py
@@ -20,5 +20,5 @@ from foods import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.HomeView.as_view())
 ]
