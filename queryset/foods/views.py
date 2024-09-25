8f428c172164903c40214c984abf96212ed03306
daniel-kav <dankavatha01@gmail.com>
2024-09-25 10:21:29 +0300

    views and routing

--- queryset/foods/views.py
+++ queryset/foods/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Food
 
 # Create your views here.
+def  home(request):
+    foods = Food.objects.all()
+    return render(request, 'home.html', {'foods': foods})
