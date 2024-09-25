1aab333c6157b50871cd6d09c785dc0bf1d2c9f5
daniel-kav <dankavatha01@gmail.com>
2024-09-25 10:12:15 +0300

    admin  reg

--- queryset/foods/admin.py
+++ queryset/foods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Food
 
-# Register your models here.
+
+# class FoodAdmin(admin.Model):...
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color', ]
+
+admin.site.register(Food, FoodAdmin)
