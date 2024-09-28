93f87c2c9c8a5dbec262b55ed6c86ed9c2562e82
daniel-kav <dankavatha01@gmail.com>
2024-09-28 09:42:02 +0300

    reverse string using stack

--- DSA/STACKS/stacks.py
+++ DSA/STACKS/stacks.py
@@ -31,3 +31,24 @@ if __name__ == '__main__':
     print (s.pop())
     print(s)
     print (s.size())
+
+def reverse_string(my_string):
+    # Use the "accumulator pattern."
+    # Start with an "empty bucket" of the right data type,
+    # and build the solution by filling the bucket within a loop.
+    reversed_string = ""
+
+    # Create a new stack
+    s = Stack()
+    # Iterate through my_string and push the characters onto the stack
+    for strin in my_string:
+        s.push(strin)
+    # Use a while loop with the exit condition that the stack is empty.
+    while not s.is_empty():
+        reversed_string += s.pop()
+    # Within this loop, update reversed_string with characters popped off the stack.
+
+    # Return the result
+    return reversed_string
+
+print(reverse_string("dan"))
