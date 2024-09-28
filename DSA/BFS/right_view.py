a151bcfab7af2fd64cf565ea47bed1859ca7d3fc
daniel-kav <dankavatha01@gmail.com>
2024-09-28 13:43:20 +0300

    binary tree right view

---
+++ DSA/BFS/right_view.py
@@ -0,0 +1,42 @@
+class Solution:
+    def right_view(self,root):
+        if root is None: return []
+
+        queue = []
+        queue.append(root)
+        result = []
+
+        while queue:
+            count = len(queue)
+
+            for i in range(count):
+                node = queue.pop(0)
+
+                if i == count - 1:
+                    result.append(node.data) # append the last node which is ussually the right node to the right view
+                if node.left :
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
+
+
+
+class Node:
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+
+# Construct the binary tree as given in the problem
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+root.left.left.right = Node(8)
+
+sol = Solution()
+print("Right view of the binary tree is:", sol.right_view(root))
