46ee0e8fd9e3097af2807db3ce97f3df606ae9de
daniel-kav <dankavatha01@gmail.com>
2024-09-29 22:01:14 +0300

    templates

--- class_proj/geeks/views.py
+++ class_proj/geeks/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render,HttpResponse
 from django.views import View
+from django.views.generic import CreateView
+from .models import SampleModel
 
 # Create your views here.
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("<h1>This is Home</h1>")
+        return render(request, 'geeks/home.html')
+    
+class GeeksCreate(CreateView):
+    #specify the model for view creation
+    model = SampleModel
+
+    #specify fields to be displayed
+    fields = ['title', 'description', ]
