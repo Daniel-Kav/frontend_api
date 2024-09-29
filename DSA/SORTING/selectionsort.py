aa0ebcee02e44e88e8cca01c1b83757e10c5c8c1
daniel-kav <dankavatha01@gmail.com>
2024-09-29 11:24:58 +0300

    select

--- DSA/SORTING/selectionsort.py
+++ DSA/SORTING/selectionsort.py
@@ -12,17 +12,34 @@
 #         arr[i], arr[min_index] = arr[min_index], arr[i]
 
 
-def selection_sort(arr):
-    n = len(arr)
+# def selection_sort(arr):
+#     n = len(arr)
+#     comparison_count = 0
 
+#     for i in range(n):
+#         min_index = i
+#         for j in range(i+1, n):
+#             comparison_count += 1
+
+#             if arr[j][1] < arr[min_index][1]:
+#                 min_index = j
+
+#         arr[i], arr[min_index] = arr[min_index], arr[i]
+#     return comparison_count
+
+def selection_sort(arr):
+
+    n = len(arr)
     for i in range(n):
+
         min_index = i
         for j in range(i+1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
-
         arr[i], arr[min_index] = arr[min_index], arr[i]
 
-arr = [64, 250, 12, 22, 11]
-selection_sort(arr)
+arr = [64, 250, 120, 22, 11,49,2,9,3,1]
+# arr = ["apple", "orange", "banana", "grape"]
+# arr = [(2, 3), (1, 5), (4, 2)]
+print(selection_sort(arr))
 print("Sorted array is:", arr)
