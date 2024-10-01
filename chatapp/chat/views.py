038b3ae0f9bf3dac0ef05659adb01bd7cedf424f
daniel-kav <dankavatha01@gmail.com>
2024-10-01 14:57:37 +0300

    chatpage

--- chatapp/chat/views.py
+++ chatapp/chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def chatPage(request, *args, **kwargs):
-    if not request.user.is_authenticated():
+    if not request.user.is_authenticated:
         return redirect("login-user")
     context = {}
     return render(request, "chat/chatPage.html", context)
