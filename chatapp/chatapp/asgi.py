47a3659daa9ec947fd990c572aa8c28bb43504df
daniel-kav <dankavatha01@gmail.com>
2024-10-02 19:48:26 +0300

    sockets and creating routings.

--- chatapp/chatapp/asgi.py
+++ chatapp/chatapp/asgi.py
@@ -17,8 +17,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter , URLRouter
+from chat import routing
 
 django_asgi_app = get_asgi_application()
 
@@ -27,6 +29,23 @@ application = ProtocolTypeRouter({
     "http": django_asgi_app,
     # Just HTTP for now. (We can add other protocols later.)
 })
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
+
+
+
+application = ProtocolTypeRouter(
+    {
+        "http" : get_asgi_application() , 
+        "websocket" : AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )    
+        )
+    }
+)
+
 
 ASGI_APPLICATION = 'chatapp.asgi.application'
 
