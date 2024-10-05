08662b1cbb13e395aaafce6c64a5dd07ac4d1902
daniel-kav <dankavatha01@gmail.com>
2024-10-05 09:55:57 +0300

    bug fixing

--- testproject/app/urls.py
+++ testproject/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     path('', views.home, name= 'home'),
-    path('new/', views.create_post,name= "new-post" )
+    path('new/', views.create_post,name= "new-post" ),
+    path('new/<int:pk>/', views.post_detail, name = "post_detail"),
 ]
--- testproject/app/views.py
+++ testproject/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Post,Comment
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -10,16 +11,34 @@ def home(request):
     }
     return render(request, 'home.html', context)
 
+# @login_required
+# def create_post(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit= False)
+#             post.user = request.user
+#             post.save()
+#             return redirect("home")
+#     else:
+#         form = PostForm()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'post.html', context)
+
+@login_required
 def create_post(request):
-    form = PostForm
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("home")
+            post = form.save(commit=False)  # Don't save yet
+            post.author = request.user  # Set the author
+            post.save()  # Now save the post
+            return redirect('home')  # Redirect to a success page or home
     else:
         form = PostForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'post.html', context)
+    return render(request, 'post.html', {'form': form})
+
+def post_detail(request, pk):
+    pass
