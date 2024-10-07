f4fededcf779cbdf75221b24338d0483f4ac9678
daniel-kav <dankavatha01@gmail.com>
2024-10-07 17:42:58 +0300

    viewsets

--- drf/todo/views.py
+++ drf/todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    #create a serializer class and assign it to the todo serializer
+    serializer_class = TodoSerializer
+    #define a var and populate it with todo data
+    queryset = Todo.objects.all()
