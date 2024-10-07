845bad1fa14224039da3e56542386f9672eb65e1
daniel-kav <dankavatha01@gmail.com>
2024-10-07 17:25:12 +0300

    white listed react port

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    'http:localhost:5173'
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'drf.urls'
