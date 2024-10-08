bcea6cdb963a2fb58f2680403df11f89d85248b0
daniel-kav <dankavatha01@gmail.com>
2024-10-08 19:35:27 +0300

    model admin intergration

--- drinks/core/admin.py
+++ drinks/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Drink
 
 # Register your models here.
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+admin.site.register(Drink, DrinkAdmin)
