85000739185c2079fb3044605c67675270821dc5
daniel-kav <dankavatha01@gmail.com>
2024-10-08 11:06:49 +0300

    add book logic

--- django-react/book-frontend/src/App.jsx
+++ django-react/book-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 const App = () => {
 
   const[books , setBooks ]= useState([])
+  const[newBook, setnewBook] = useState({ title: '', description :'', read : false})
 
   const getBooks = () => {
     axios.get('http://127.0.0.1:8000/api/books/')
@@ -13,12 +14,44 @@ const App = () => {
       console.error('There was an error fetching books', error)
     })
   }
+
+  const addBook = () => {
+    axios.post('http://localhost:8000/api/books/', newBook)
+    .then(()=> {
+      getBooks()
+      setnewBook({ title: '', description: '', read: false})
+    })
+    .catch( error => {
+      console.error( 'Error adding book ', error)
+    })
+  }
+
   useEffect(() => {
     getBooks()
   },[])
   return (
     <div>
       <h1> My frontend </h1>
+      <h2>Add Book</h2>
+      <input type="text" 
+        placeholder='Title'
+        value={newBook.title}
+        onChange={(e) => setnewBook({...newBook, title: e.target.value})}
+      />
+
+      <input type="text" 
+        placeholder='description'
+        value={newBook.description}
+        onChange={(e) => setnewBook({...newBook, description: e.target.value})}
+      />
+      <label htmlFor="">
+        <input type="checkbox" 
+          checked = {newBook.checked}
+          onChange={(e) => setnewBook({...newBook, read: e.target.checked})}
+        />
+        Read
+      </label>
+      <button onClick={() => addBook()}>Add Book</button>
       <ul>
         { books.map(book =>(
           <li key={book.id}>
