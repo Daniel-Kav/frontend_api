7366db9855cda17188de3e046a3711dc4d81ccd3
daniel-kav <dankavatha01@gmail.com>
2024-10-08 19:40:52 +0300

    viewsets done

--- drinks/core/views.py
+++ drinks/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Drink
+from .serializers import DrinkSerializer
 
 # Create your views here.
+
+class DrinkView(viewsets.ModelViewSet):
+    serializer_class = DrinkSerializer
+    queryset = Drink.objects.all()
