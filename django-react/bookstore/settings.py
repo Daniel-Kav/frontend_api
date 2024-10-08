f9d148df77328d5fd244dbe77357ba77b834d7d5
daniel-kav <dankavatha01@gmail.com>
2024-10-08 08:32:18 +0300

    app setupt

--- django-react/bookstore/settings.py
+++ django-react/bookstore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-69%yyh&2jgmkljr-+wz13jg9gw4_j%_&dkk=wuwhih0obulenh'
+SECRET_KEY = 'django-insecure-2sys2n(3j5*fwpml0qxcc4a1$-wdo-tw)oj2nbp*3su^=l$4mg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
