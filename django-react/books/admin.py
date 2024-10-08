333a7f83a58076cc566170e2ce042ebb3fa9b28f
daniel-kav <dankavatha01@gmail.com>
2024-10-08 08:44:17 +0300

    model admin intergration

--- django-react/books/admin.py
+++ django-react/books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+admin.site.register(Book, BookAdmin)
