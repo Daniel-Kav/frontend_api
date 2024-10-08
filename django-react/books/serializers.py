d884e8a5648cc330ca7fa809d7b208b8429fb86e
daniel-kav <dankavatha01@gmail.com>
2024-10-08 09:03:23 +0300

    bbok view logic

--- django-react/books/serializers.py
+++ django-react/books/serializers.py
@@ -2,8 +2,7 @@ from rest_framework import serializers
 from .models import Book
 
 
-class BookSerializers(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-        
