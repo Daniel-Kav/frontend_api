03a92c98cb8ca9868bb2870c0b782b7cbdef6d7b
daniel-kav <dankavatha01@gmail.com>
2024-10-08 08:54:16 +0300

    cors headers cnfig

--- django-react/bookstore/settings.py
+++ django-react/bookstore/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000'
 ]
 
 ROOT_URLCONF = 'bookstore.urls'
