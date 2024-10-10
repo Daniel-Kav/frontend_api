d0325cf6cc2bec8bb3d05f79e9f412535df40637
daniel-kav <dankavatha01@gmail.com>
2024-10-10 11:18:26 +0300

    GET POST endpoints done

--- recipe_sharing/recipes/views.py
+++ recipe_sharing/recipes/views.py
@@ -3,10 +3,18 @@ from .serializers import RecipeSerializer
 from .models import Recipe
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.decorators import api_view
 # Create your views here.
-
+@api_view(['GET', 'POST'])
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    serializer = RecipeSerializer(recipes, many= True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        serializer = RecipeSerializer(recipes, many= True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        serializer = RecipeSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
