957434012dec73f16c757f3883fff43b57a6f37b
daniel-kav <dankavatha01@gmail.com>
2024-10-10 11:05:29 +0300

    debuging renderer issue

--- recipe_sharing/recipes/views.py
+++ recipe_sharing/recipes/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .serializers import RecipeSerializer
+from .models import Recipe
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 def recipe_list(request):
-    pass
+    recipes = Recipe.objects.all()
+    serializer = RecipeSerializer(recipes, many= True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
