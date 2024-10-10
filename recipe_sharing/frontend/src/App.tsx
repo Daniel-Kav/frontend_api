ef830196edb87b329af6c3eb8ab06eafd2d18539
daniel-kav <dankavatha01@gmail.com>
2024-10-10 12:50:12 +0300

    added recipes to the backend from the frontend successfully

--- recipe_sharing/frontend/src/App.tsx
+++ recipe_sharing/frontend/src/App.tsx
@@ -7,10 +7,12 @@ interface Recipe{
   title: string,
   description: string,
   instructions: string,
+  ingredients: string,
 }
 
 const App = () => {
   const[recipes, setRecipes] = useState([])
+  const[newRecipe, setNewRecipe] = useState({title: '',description:'',instructions:'',ingredients:''})
 
   const getRecipes = () => {
     axios.get('http://127.0.0.1:8000/api/recipes/')
@@ -20,6 +22,18 @@ const App = () => {
     })
   }
 
+  const addRecipe = () => {
+    axios.post('http://127.0.0.1:8000/api/recipes/', {...newRecipe,author: 1})
+    .then(() => {
+      getRecipes()
+      setNewRecipe({title: '',description:'',instructions:'',ingredients:''})
+    })
+  }
+
+  const deleteRecipe = () => {
+    
+  }
+
   useEffect(() => {
     getRecipes()
   },[])
@@ -27,10 +41,33 @@ const App = () => {
   return (
     <div>
       <h1>Recipes Garlour</h1>
+      <input type="text" 
+        placeholder='title'
+        value={newRecipe.title}
+        onChange={(e) => setNewRecipe({...newRecipe, title: e.target.value})}
+      />
+      <input type="text" 
+        placeholder='description'
+        value={newRecipe.description}
+        onChange={(e) => setNewRecipe({...newRecipe, description: e.target.value})}
+      />
+      <input type="text" 
+        placeholder='ingredients'
+        value={newRecipe.ingredients}
+        onChange={(e) => setNewRecipe({...newRecipe, ingredients: e.target.value})}
+      />
+      <input type="text" 
+        placeholder='instructions'
+        value={newRecipe.instructions}
+        onChange={(e) => setNewRecipe({...newRecipe, instructions: e.target.value})}
+      />
+      <button onClick={() => addRecipe()}>Add Recipe</button>
+
       {recipes && recipes.map((recipe: Recipe) => (
         <li key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+          <p>{recipe.ingredients}</p>
           <p>{recipe.instructions}</p>
         </li>
       )) }
