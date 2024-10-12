e5fbf980616800fc9b8e23c0083bf1c3ed3472aa
daniel-kav <dankavatha01@gmail.com>
2024-10-12 10:12:01 +0300

    list and create publishers

--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Publisher,Author,Book
+from .serializers import PublisherSerializer,AuthorSerializer,BookSerializer
 
 # Create your views here.
+# list and create publishers
+class PublisherCreateView(generics.ListCreateAPIView):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+
