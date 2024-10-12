65e5e2200a6b11f569a35a244306a3cc81357c9a
daniel-kav <dankavatha01@gmail.com>
2024-10-12 09:56:02 +0300

    model admin reg

--- bookstore/store/admin.py
+++ bookstore/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from rest_framework import serializers
+from .models import Publisher,Author,Book
 
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
