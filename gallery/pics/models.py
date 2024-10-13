820c824e0dcd5154772de653c0400f09d2f920f4
daniel-kav <dankavatha01@gmail.com>
2024-10-13 20:05:19 +0300

    product model

--- gallery/pics/models.py
+++ gallery/pics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
