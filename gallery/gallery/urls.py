fd0da700599e65942981a44d85b8dad0b599ea95
daniel-kav <dankavatha01@gmail.com>
2024-10-13 20:36:18 +0300

    successfully added the images

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path,include
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    ...]
+    path('',include('pics.urls')),
+    ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
