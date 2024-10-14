2331e6fc36ba2d1f8110751c089d5cb0d8026e30
daniel-kav <dankavatha01@gmail.com>
2024-10-14 09:50:21 +0300

    add product view

--- gallery/pics/views.py
+++ gallery/pics/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'pics/index.html',{'products': products})
+
+def add_product(request):
+    form = ProductForm
+    if request.method == 'POST':
+        form = ProductForm(request.data)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request,'pics/pform.html',{'form': form})
