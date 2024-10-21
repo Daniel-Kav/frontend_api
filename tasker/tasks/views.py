80bc8c0936dc45ec98e3d0a8dcfd015c658ac533
daniel-kav <dankavatha01@gmail.com>
2024-10-21 18:11:34 +0300

    detail view

--- tasker/tasks/views.py
+++ tasker/tasks/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Task
 
 # Create your views here.
 def task_list(request):
     tasks = Task.objects.all()
     return render(request,'tasks.html',{'tasks': tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    return(request, 'detail.html',{'task': task})
