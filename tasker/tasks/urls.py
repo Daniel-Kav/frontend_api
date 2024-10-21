a3f8bae986124c95d0bc36c0431d0f5227626c8d
daniel-kav <dankavatha01@gmail.com>
2024-10-21 18:34:49 +0300

    update done

--- tasker/tasks/urls.py
+++ tasker/tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.task_list,name='task_list'),
     path('task/<int:pk>/',views.task_detail, name='task_detail'),
+    path('task/<int:pk>/update/',views.task_update, name = 'task_update'),
 ]
