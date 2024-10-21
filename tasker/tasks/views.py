eb0931e49f0df51cf340ba994c227f826b54cde9
daniel-kav <dankavatha01@gmail.com>
2024-10-21 18:26:29 +0300

    task model form

--- tasker/tasks/views.py
+++ tasker/tasks/views.py
@@ -10,3 +10,8 @@ def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
 
     return render(request, 'detail.html',{'task': task})
+
+def task_update(request,pk):
+    task = get_object_or_404(Task, pk = pk)
+    if request.method == 'POST':
+        
