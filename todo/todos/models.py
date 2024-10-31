e478d6db267a2d35283d56c1e7b126b98714a2ea
daniel-kav <dankavatha01@gmail.com>
2024-10-31 18:07:34 +0300

    url config

--- todo/todos/models.py
+++ todo/todos/models.py
@@ -6,4 +6,7 @@ class TaskModel(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateField(auto_now_add= True)
+
+class TaskModel(models.Model):
+    pass
     
