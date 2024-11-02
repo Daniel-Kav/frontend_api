9fc560e05a46e7d495495ec36b8871a52237e9f3
daniel-kav <dankavatha01@gmail.com>
2024-11-02 10:32:27 +0300

    PUT done

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add-task/', views.add_task, name='add_task'),
+    path('edit-task/<int:pk>', views.edit_task, name='edit_task'),
 ]
