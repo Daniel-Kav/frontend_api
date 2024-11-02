9b4b94da64e0ff15c911ad6e28033ac23d57b427
daniel-kav <dankavatha01@gmail.com>
2024-11-02 10:40:34 +0300

    DELETE done

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add-task/', views.add_task, name='add_task'),
     path('edit-task/<int:pk>', views.edit_task, name='edit_task'),
+    path('del-task/<int:pk>', views.del_task, name='del_task'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -31,6 +31,6 @@ def edit_task(request,pk):
 def del_task(request,pk):
     task = get_object_or_404(Task,pk=pk)
     if request.method == 'POST':
-        task.delete
+        task.delete()
         return redirect('task_list')
     return render(request,'del.html',{'task': task})
