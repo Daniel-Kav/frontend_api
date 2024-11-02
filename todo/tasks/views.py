fc130b36c59893fed49c8f533048d20cc1653124
daniel-kav <dankavatha01@gmail.com>
2024-11-02 10:26:51 +0300

    edit logic view

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from  .models  import Task
 from .forms import TaskForm
 
@@ -16,3 +16,14 @@ def add_task(request):
     else:
         form = TaskForm
     return render(request,'add_task.html',{'form': form})
+
+def edit_task(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance= task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request,'edit_task.html',{'form': form})
