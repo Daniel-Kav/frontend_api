df09cd735617157ec5e38f34801269c54561cc8e
daniel-kav <dankavatha01@gmail.com>
2024-11-05 09:56:19 +0300

    task serializers

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from  .models  import Task
 from .forms import TaskForm
 
+
 # Create your views here
 def task_list(request):
     tasks = Task.objects.all()
