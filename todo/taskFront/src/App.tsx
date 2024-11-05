bddcbcdce2c3b47eb934106285f27afe03bf79b6
daniel-kav <dankavatha01@gmail.com>
2024-11-05 18:30:23 +0300

    geting data from backend

--- todo/taskFront/src/App.tsx
+++ todo/taskFront/src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react"
+import axios, { AxiosResponse } from 'axios'
+
+type Task = {
+  description: string
+  title: string
+}
 
 const App = () => {
-  const[count, setCount ] = useState(0)
+  const[tasks, setTasks] = useState<Task[]>([])
 
-
+  const getTasks = () => {
+    axios.get('http://127.0.0.1/tasks')
+    .then((res: AxiosResponse) => {
+      setTasks(res.data)
+    })
+  }
 
   return (
     <div>
