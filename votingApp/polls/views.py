215e319f1ac27df42433cbae8f6007c268e69836
daniel-kav <dankavatha01@gmail.com>
2024-11-08 13:45:20 +0300

    desine views

--- votingApp/polls/views.py
+++ votingApp/polls/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404,redirect
+from .models import Question,Choice
 
 # Create your views here.
 def sampleView(request):
     return HttpResponse("Helo this worked")
+
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_questions': latest_questions})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls:results', question_id=question.id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
