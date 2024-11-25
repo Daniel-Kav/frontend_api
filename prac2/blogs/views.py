b5a54e59f959137a3a716bc35cf6df40ad89a312
daniel-kav <dankavatha01@gmail.com>
2024-11-25 18:50:46 +0300

    home template

--- prac2/blogs/views.py
+++ prac2/blogs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+def getBlogs(request):
+    blogs = Blog.objects.all()
+    return render(request,'home.html',{blogs:'blogs'})
+
