d554fa114c4ef6c5a8f44a3ca6de908f602d2127
daniel-kav <dankavatha01@gmail.com>
2024-11-26 08:30:01 +0300

    edit template

--- prac2/blogs/urls.py
+++ prac2/blogs/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.getBlogs, name='home'),
-    path('new-blog/',views.addBlog, name='addBlog')
+    path('new-blog/',views.addBlog, name='addBlog'),
+    path('edit-blog/',views.editBlog,name='editblog')
+
 ]
