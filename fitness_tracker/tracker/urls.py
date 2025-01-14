77d27b6fca657a311c61a75caf0dd3d3d5419bf1
daniel-kav <dankavatha01@gmail.com>
2025-01-14 10:44:57 +0300

    API routing for user profiles, workouts, meals, and goals

--- fitness_tracker/tracker/urls.py
+++ fitness_tracker/tracker/urls.py
@@ -1,6 +1,19 @@
-from django.urls import path
+from django.urls import path,include
 from .views import index
+from rest_framework.routers import DefaultRouter
+from tracker import views
+
+
+
+router = DefaultRouter()
+router.register(r'user_profiles', views.UserProfileViewSet)
+router.register(r'workouts', views.WorkoutViewSet)
+router.register(r'meals', views.MealViewSet)
+router.register(r'goals', views.GoalViewSet)
 
 urlpatterns = [
     path('', index, name='index'),
+    path('api/', include(router.urls)),
 ]
+
+
