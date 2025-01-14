11641f273551959897e9cef7ec319616b3d014fb
daniel-kav <dankavatha01@gmail.com>
2025-01-14 10:40:10 +0300

    viewsets for user profile, workout, meal, and goal models

--- fitness_tracker/tracker/views.py
+++ fitness_tracker/tracker/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'base.html')
+
+from rest_framework import viewsets
+from .models import UserProfile, Workout, Meal, Goal
+from .serializers import UserProfileSerializer, WorkoutSerializer, MealSerializer, GoalSerializer
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
+
+class GoalViewSet(viewsets.ModelViewSet):
+    queryset = Goal.objects.all()
+    serializer_class = GoalSerializer
+
