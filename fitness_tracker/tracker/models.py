d0ee58442c6ac68cec9bd3546f0aac10a22e708c
daniel-kav <dankavatha01@gmail.com>
2025-01-14 10:37:00 +0300

    add user profile and fitness tracking models

--- fitness_tracker/tracker/models.py
+++ fitness_tracker/tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,3 +9,32 @@ class Tracker(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    height = models.FloatField()  # in cm
+    weight = models.FloatField()  # in kg
+    fitness_goal = models.CharField(max_length=255)
+
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    exercise = models.CharField(max_length=255)
+    duration = models.DurationField()  # in minutes
+    calories_burned = models.FloatField()
+
+class Meal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    meal_name = models.CharField(max_length=255)
+    calories = models.FloatField()
+
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    target_weight = models.FloatField()
+    
